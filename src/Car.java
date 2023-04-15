8e4e219790248ce9da24d1795eb3292777243b80
dariamilk <daryachulpanova@gmail.com>
2023-04-15 21:15:56 +0300

    ДЗ урока 2.3

--- src/Car.java
+++ src/Car.java
@@ -1,13 +1,17 @@
-public class Car {
+public class Car extends Vehicle {
 
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
-
     public void checkEngine() {
         System.out.println("Проверяем двигатель");
+    }
+    @Override
+    public void service() {
+        System.out.println("Обслуживаем " + super.getModelName());
+        for (int i = 0; i < super.getWheelsCount(); i++) {
+            super.updateTyre();
+        }
+        checkEngine();
     }
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,19 @@
+public abstract class Vehicle implements Serviceable {
+    private final String modelName;
+    private final int wheelsCount;
+    public Vehicle (String modelName, int wheelsCount) {
+            this.modelName = modelName;
+            this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
